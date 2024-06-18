a7414423b91168b3469778ecbbca8e9ce29e2374
THARUN G <tharun26.g@gmail.com>
2024-06-18 16:53:33 +0530

    footer commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import TopNav from "./components/TopNav";
+import Footer from './components/Footer';
+import './index.css';
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-    <TopNav />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-blue-500 text-white text-center p-4">
+        {/* Your header content */}
+        <h1>Welcome to Tonbo Imaging</h1>
+      </header>
+      <main className="flex-grow mb-12">
+        {/* Your main content */}
+        <div className="container mx-auto p-4">
+          <p>Your main content goes here.</p>
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
