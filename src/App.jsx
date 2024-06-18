ec9596eab8cb01e0b41c43a819487e274ca5d8c3
Kishorebalaji19 <kishorebalaji1912@gmail.com>
2024-06-18 15:00:54 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import TopNav from "./components/TopNav";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <TopNav />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
---
+++ src/components/TopNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopNav = () => {
+  return (
+    <div>
+        <div className='text-3xl'>Top Nav</div>
+    </div>
+  )
+}
+
+export default TopNav
